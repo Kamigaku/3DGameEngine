def0f8883c2040792002f3718567d6eda3746851
Kamigaku <kamigaku@gmail.com>
2019-10-09 21:24:03 +0200

    Fix #1 by updating the entity's update logic

--- 3DGameEngine/Entities/AEntity.cs
+++ 3DGameEngine/Entities/AEntity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameEngine.Model;
+﻿using GameEngine.Model;
 using Microsoft.Xna.Framework;
 
 namespace GameEngine.Entities
